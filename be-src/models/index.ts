94862dee7d0c46063c5f5c438fdeef3c64a2b1c8
Matias <matias.parodi@outlook.com>
2022-02-08 14:27:53 -0300

    sequelize enenviromental variables

--- be-src/models/index.ts
+++ be-src/models/index.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 export const sequelize = new Sequelize({
   dialect: "postgres",
   username: "pzadprxdazemxn",
-  password: "8da68df538b9dc9ca051a69bb957161dcaecbd7a3e80e9756b0f13a86dcbf042",
+  password: process.env.SEQUELIZE_PASSWORD,
   database: "de769880lt2m2j",
   port: 5432,
   host: "ec2-18-235-235-165.compute-1.amazonaws.com",
